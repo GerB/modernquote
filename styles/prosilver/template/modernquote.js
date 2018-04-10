521437b26dc8071e76f5924119ddcaed8b554a60
Ger Bruinsma <ger_bruinsma@hotmail.com>
2018-04-10 11:36:17 +0200

    Optimize JS

--- styles/prosilver/template/modernquote.js
+++ styles/prosilver/template/modernquote.js
@@ -12,11 +12,10 @@
 	// Gather data
 	var post = $(this).parents('.post');
 	var post_id = post.prop('id').replace(/[^0-9]/g, '');
-	var username = post.find('.username-coloured').first().text();
-	var poster_id = post.find('.row_poster_id').text();
-	var post_time = post.find('.row_post_time').text();
-        var forum_id = $('#nav-breadcrumbs .crumb').last().attr('data-forum-id');
         if ($('.floatquote').hasClass('qr')) {
+            var username = post.find('.username-coloured').first().text();
+            var poster_id = post.find('.row_poster_id').text();
+            var post_time = post.find('.row_post_time').text();
             addquote(post_id, username, '{LA_WROTE}', {post_id:post_id, time:post_time, user_id:poster_id});
             $('.floatquote').remove();
         } else {
